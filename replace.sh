0160c54150bdc41bdd172db9500f3af32437cb31
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2024-10-17 14:40:45 +0330

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -7,7 +7,7 @@ rm -f /var/www/html/wizwizxui-timebot/settings/values.php
 echo "Deleting values.php..."
 sleep 2
 
-curl -o /var/www/html/wizwizxui-timebot/settings/values.php https://raw.githubusercontent.com/ItsOrv/simple-wiz/main/settings/values.php
+curl -o /var/www/html/wizwizxui-timebot/settings/values.php https://raw.githubusercontent.com/MrAminiDev/BetterLang-wiz/main/settings/values.php
 echo "Replaced values.php."
 
 echo "All files have been successfully replaced."
