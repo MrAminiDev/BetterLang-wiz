d1c510eb1e355dde3ed1b8b3fecb169491dba90d
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2024-10-17 22:07:12 +0330

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -10,4 +10,11 @@ sleep 2
 curl -o /var/www/html/wizwizxui-timebot/settings/values.php https://raw.githubusercontent.com/MrAminiDev/BetterLang-wiz/main/settings/values.php
 echo "Replaced values.php."
 
+rm -f /var/www/html/wizwizxui-timebot/settings/warnusers.php
+echo "Deleting warnusers.php..."
+sleep 2
+
+curl -o /var/www/html/wizwizxui-timebot/settings/warnusers.php https://raw.githubusercontent.com/MrAminiDev/BetterLang-wiz/main/settings/warnusers.php
+echo "Replaced warnusers.php."
+
 echo "All files have been successfully replaced."
