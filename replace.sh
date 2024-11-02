28e25351077f7d247c6ff624b2104207852640cd
MAN.NJFZ <man.njfz@gmail.com>
2024-11-02 13:53:10 -0700

    Update replace.sh
    Add task to replace bot.php file

--- replace.sh
+++ replace.sh
@@ -17,4 +17,11 @@ sleep 2
 curl -o /var/www/html/wizwizxui-timebot/settings/warnusers.php https://raw.githubusercontent.com/MrAminiDev/BetterLang-wiz/main/settings/warnusers.php
 echo "Replaced warnusers.php."
 
+rm -f /var/www/html/wizwizxui-timebot/bot.php
+echo "Deleting bot.php..."
+sleep 2
+
+curl -o /var/www/html/wizwizxui-timebot/bot.php https://raw.githubusercontent.com/MrAminiDev/BetterLang-wiz/main/bot.php
+echo "Replaced bot.php."
+
 echo "All files have been successfully replaced."
